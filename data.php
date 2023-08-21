2d63f06f11bb8fd1402373d4144bf3a1e44c5ab9
Samikshya Timalsina <hce076bct034@hcoe.edu.np>
2023-08-21 18:22:22 +0545

    Changes made

--- data.php
+++ data.php
@@ -4,10 +4,11 @@ $age = $_POST["age"];
 $email = $_POST["email"];
 $message = $_POST["message"];
 
-$db="dbase";
+
 $server="localhost";
 $username="root";
 $password="";
+$db="dbase";
 
 //connection to database
 $conn=mysqli_connect($server,$username,$password,$db);
@@ -18,6 +19,17 @@ if($conn)
 else
 {
     echo 'connection failed';
+}
+
+$sql="INSERT INTO `contact_info`(`name`, `age`, `email`, `message`) VALUES ('$name','$age','$email','$message')";
+$result=mysqli_query($conn,$sql);
+if(isset($result))
+{
+    echo'Data inserted successfully';
+}
+else
+{
+    echo'Data inserted. Try Again!';
 }
 mysqli_close($conn);
 ?>
